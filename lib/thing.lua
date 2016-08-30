499d2b6ad54227494c43cc49e5ad96ec0691bcdf
Lord Niels N. Horn <i.am@nilq.dk>
2016-08-30 17:21:23 +0200

    Add class implement event on thing.lua

--- lib/thing.lua
+++ lib/thing.lua
@@ -1,9 +1,17 @@
 Thing = setmetatable({}, {
   __call = function(parent)
     local thing = setmetatable({
-      newValue   = Property.newValue,
-      newBoolean = Property.newBoolean,
-      newTable   = Property.newTable,
+      newValue        = Property.newValue,
+      newBoolean      = Property.newBoolean,
+      newTable        = Property.newTable,
+      newEvent        = Event.new,
+      emitEvent       = Event.emit,
+      removeEvent     = Event.remove,
+      removeSubEvent  = Event.removeSub,
+      setSubEvent     = Event.setSub,
+      enableSubEvent  = Event.enableSub,
+      disableSubEvent = Event.disableSub,
+      toggleSubEvent  = Event.toggleSub,
     }, {
       __index = parent,
     })
